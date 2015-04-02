52125a8673394daacde5194e949676904a79b815
Vitor Spencer <vitorspencer@gmail.com>
2015-04-02 09:47:21 +0200

    Initial Commit

---
+++ Slot Machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slot Machine
+//
+//  Created by Vitor on 02/04/15.
+//  Copyright (c) 2015 Vitor Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
